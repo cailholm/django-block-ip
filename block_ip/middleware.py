43ec2137a8298d47553ce56bd67786edd90ca004
cailholm <cailholm@thetys-france.fr>
2019-06-18 17:35:02 +0200

    Bugfix header name

--- block_ip/middleware.py
+++ block_ip/middleware.py
@@ -6,7 +6,7 @@ from .models import BlockIP
 
 
 def get_ip(req):
-    x_forwarded_for = req.META.get('X_FORWARDED_FOR')
+    x_forwarded_for = req.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
